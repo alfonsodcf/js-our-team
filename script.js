bb96aab4f04ba3fb4d341274bd447fa229d83399
alfonsodcf <alfonsoservizi01@gmail.com>
2022-04-07 17:38:42 +0200

    script

---
+++ script.js
@@ -0,0 +1,63 @@
+/* <div class="team-card">
+    <div class="card-image">
+    <img
+        src="img/wayne-barnett-founder-ceo.jpg"
+        alt="Wayne Barnett"
+    />
+    </div>
+    <div class="card-text">
+        <h3>Wayne Barnett</h3>
+        <p>Founder & CEO</p>
+    </div>
+</div>  */
+
+
+card = [
+    {
+        img: 'wayne-barnett-founder-ceo.jpg',
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO',
+    },
+
+    {
+        img: 'angela-caroll-chief-editor.jpg',
+        nome: 'Angela Caroll',
+        ruolo: 'Chief Editor',
+    },
+
+    {
+        img: 'walter-gordon-office-manager.jpg',
+        nome: 'Walter Gordon',
+        ruolo: 'Office Manager',
+    },
+
+    {
+        img: 'angela-lopez-social-media-manager.jpg',
+        nome: 'Angela LOpez',
+        ruolo: 'Social Media Manager',
+    },
+
+    {
+        img: 'scott-estrada-developer.jpg',
+        nome: 'Scott Estrada',
+        ruolo: 'Developer',
+    },
+
+    {
+        img: 'barbara-ramos-graphic-designer.jpg',
+        nome: 'Barbara Ramos',
+        ruolo: 'Graphic Designer',
+    },
+    
+]
+
+
+console.log(card[1].img);
+
+
+function stampaCard(){
+    document.getElementById('')  
+    for(let i = 1; i < card.length; i++){
+        
+    }
+}
